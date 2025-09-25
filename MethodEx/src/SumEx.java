f87c92c7ca25efecae9bdc7f950a336a974578ac
nirav <nirav@192.168.111.13>
2025-09-25 11:26:47 -0400

    Testing Java Program to GitHub

---
+++ MethodEx/src/SumEx.java
@@ -0,0 +1,22 @@
+public class SumEx 
+{
+	public void addition()
+	{
+		int no1=5;
+		int no2=7;
+		int result = no1 +no2;
+		
+		System.out.println("Sum of two Number " + result);
+		
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+	
+		SumEx S = new SumEx();
+		S.addition();
+		
+	}
+
+}
